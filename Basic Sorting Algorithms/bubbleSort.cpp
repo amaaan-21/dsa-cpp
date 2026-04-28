424e69569d2a299f7b065c9f6f94d3f78afa56b8
amaaan <amaaan@Mac.lan>
2026-04-28 02:03:13 -0600

    Bubble sort

--- Basic Sorting Algorithms/bubbleSort.cpp
+++ Basic Sorting Algorithms/bubbleSort.cpp
@@ -13,14 +13,40 @@ void printArr(int *arr, int n){
 void bubbleSort(int *arr, int n){
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n-i-1; j++)
-        if(arr[j]>arr[j+1]){
+        //ascending order
+         if(arr[j]>arr[j+1]){
             std::swap(arr[j],arr[j+1]);
         }
+        // //decending order
+        // if(arr[j]<arr[j+1]){
+        //     std::swap(arr[j],arr[j+1]);
+        // }
         
     }
      printArr(arr, n);
 
 }
+
+/*optimize
+void bubbleSort(int *arr, int n){
+    bool isSwap = false
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-i-1; j++)
+        //ascending order
+         if(arr[j]>arr[j+1]){
+            std::swap(arr[j],arr[j+1]);
+            isSwap = true;
+        }
+       
+        
+    }
+        if(!isSwap){
+        //array already sorted no need to sort
+        return;
+        }
+    
+
+}*/
 
 int main(){
     int arr[5]={5,4,2,3,1};
