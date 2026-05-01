5b5eeed5843c98fb363512a0ee6c308108c449a3
amaaan <amaaan@amaaans-MacBook-Pro.local>
2026-04-30 21:39:48 -0600

    char string

---
+++ char&string array/char.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//strln use to print length of the array
+
+int main(){
+    char work[50] ={'h','e','l','l','o'};
+    char w[]={"hello"};
+    cout<<w<<endl;
+    cout<<strlen(work)<<endl;
+    cout<<work<<endl;
+
+    char input[50];
+    cin>>input;//ignore whitespaces
+    cout<<"your word is : "<<input<<endl;
+    cout<<"you length of char is : "<<strlen(input)<<endl;
+    char sentenceInput[60];
+    cin.getline(sentenceInput, 50);//to print space also
+    cout<<"your add for sentence is : "<<sentenceInput<<endl;
+    cout<<"your length of sentences is : "<<strlen(sentenceInput)<<endl;
+
+    return 0;
+}
