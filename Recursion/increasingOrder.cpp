c8cc5c982629ddb898ea7177981cbc5e7762f68c
amaaan <amaaan@Mac.lan>
2026-06-28 02:47:56 -0600

    problem 1 to N Rec

--- Recursion/increasingOrder.cpp
+++ Recursion/increasingOrder.cpp
@@ -1,14 +1,20 @@
+/*Write a recursive function that 
+//prints numbers from 1 to N*/
+
 #include <iostream>
 using namespace std;
-void print (int n ){
-    
-    if(n==0){//base case
+void print1ToN(int n){
+    //base case
+    if(n==0){
         return;
-    }print(n-1);//assumption 
-    cout<<(n)<<" ";//work
     
-};
-int main(){
-    print(7);
+}
+    //recursion
+    print1ToN(n-1);
+    //work
+    cout<<n<<", ";
+}
+int main() {
+    print1ToN(9);
     return 0;
 }
