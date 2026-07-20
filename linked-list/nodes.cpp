b412fa917ebaf4db62e4831187448046682eb89a
Amanullah Siddique <siddique@uleth.ca>
2026-07-20 14:52:00 -0600

    nodes

--- linked-list/nodes.cpp
+++ linked-list/nodes.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 class Node{
+public:
     int data;
     Node* next;
-puplic:
+
     Node(int val){
         data = val;
         next = NULL;
@@ -15,15 +16,53 @@ puplic:
 class List{
     Node* head;
     Node* tail;
-public;
+public:
     List(){
         head = NULL;
         tail = NULL;
     }
+void push_front(int val){
+    Node* newNode = new Node(val);//dynamic
+    
+    if(head == NULL){
+        head = tail = newNode;
+    } else {
+        newNode -> next = head; //newNode.next
+        head = newNode;
+        }
+    }
+ void push_back(int val){
+    Node* newNode= new Node(val);
+    if(head== NULL) {
+        head = tail = newNode;
+    } else {
+        tail -> next = newNode;
+        tail = newNode;
+    }
+}
+void printList(){
+    Node* temp = head;
+
+    while(temp != NULL){
+        cout<<temp -> data <<" ";
+        temp = temp -> next;
+    }
+}
+
+       
 };
 
 int main () {
-
+    List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.printList();
+    cout<<endl;
+    ll.push_back(6);
+    ll.push_back(7);
+    ll.push_front(9);
+    ll.printList();
 
     return 0;
 }
