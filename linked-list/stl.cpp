01863da841e78d64612f115646ce88932664d7ce
Amanullah Siddique <siddique@uleth.ca>
2026-09-11 01:58:19 -0600

    stl

---
+++ linked-list/stl.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<list>
+#include<iterator>
+using namespace std;
+
+void printList(list<int>ll){
+    list<int> ::iterator itr;
+    for(itr = ll.begin(); itr != ll.end(); itr++){
+        cout<<(*itr)<<" ->" ;
+    }
+    cout<<endl;
+}
+
+
+int main(){
+    list<int>ll;
+    ll.push_back(9);
+    ll.push_front(10);
+    printList(ll);
+    cout<<endl;
+    cout<<ll.size();
+
+    return 0;
+}
